2e3305cd8caa2778b02ed4fdeb7e15faf1a2f0db
Austin <austin.wang1357@gmail.com>
2022-09-14 17:42:18 -0400

    debug

--- workflow/scripts/seurat_integration_qc.R
+++ workflow/scripts/seurat_integration_qc.R
@@ -17,15 +17,15 @@ proj <- readRDS(file = input_paths[["project_in"]])
 head(proj@meta.data) ####
 
 proj$log_counts <- log10(proj$nCount_RNA)
-plt <- FeaturePlot(proj_merged, reduction = "umap", features = "log_counts") + labs(title="Post-Harmony log10 UMIs")
+plt <- FeaturePlot(proj, reduction = "umap", features = "log_counts") + labs(title="Post-Harmony log10 UMIs")
 ggsave(output_paths[["umap_counts"]], plt, device = "pdf", width = 10, height = 7)
 
 doubletfinder_col <- grep("pANN", names(proj@meta.data), value = TRUE)
-plt <- FeaturePlot(proj_merged, reduction = "umap", features = doubletfinder_col) + labs(title="Post-Harmony Doubletfinder pANN")
+plt <- FeaturePlot(proj, reduction = "umap", features = doubletfinder_col) + labs(title="Post-Harmony Doubletfinder pANN")
 ggsave(output_paths[["umap_doubletfinder"]], plt, device = "pdf", width = 10, height = 7)
 
 proj$amulet_nlp <- -log10(proj$amulet_pval)
-plt <- FeaturePlot(proj_merged, reduction = "umap", features = "log_counts") + labs(title="Post-Harmony Amulet -log10 p-Value")
+plt <- FeaturePlot(proj, reduction = "umap", features = "log_counts") + labs(title="Post-Harmony Amulet -log10 p-Value")
 ggsave(output_paths[["umap_amulet"]], plt, device = "pdf", width = 10, height = 7)
 
 sink(type = "message")
