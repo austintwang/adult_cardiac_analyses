bbecb793d59de90480ca3a65bbbe4ccdcc18fdd7
Austin <austin.wang1357@gmail.com>
2022-09-15 13:55:28 -0400

    debug

--- workflow/scripts/seurat_build_reference_teichmann.R
+++ workflow/scripts/seurat_build_reference_teichmann.R
@@ -9,13 +9,13 @@ library(Seurat)
 library(patchwork)
 library(ggplot2)
 
-library(SeuratData, lib.loc=input_paths[["azimuth_library_dir"]])
-library(SeuratDisk, lib.loc=input_paths[["seuratdisk_library_dir"]])
-
 params <- snakemake@params
 input_paths <- snakemake@input
 output_paths <- snakemake@output
 log_paths <- snakemake@log
+
+library(SeuratData, lib.loc=input_paths[["azimuth_library_dir"]])
+library(SeuratDisk, lib.loc=input_paths[["seuratdisk_library_dir"]])
 
 set.seed(params[["seed"]])
 
