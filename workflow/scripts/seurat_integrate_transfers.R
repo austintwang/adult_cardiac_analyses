fd500452ce3597a0c221fabcd448e1df86b9315b
Austin <austin.wang1357@gmail.com>
2022-09-15 18:07:03 -0400

    debug

--- workflow/scripts/seurat_integrate_transfers.R
+++ workflow/scripts/seurat_integrate_transfers.R
@@ -18,6 +18,7 @@ read_fn <- function(path, sample) {
 }
 
 tables <- mapply(read_fn, input_paths[["tables"]], params[["samples"]])
+head(tables[[1]]) ####
 data_stacked <- do.call(rbind, tables)
 
 write.table(data_stacked, file = output_paths[["data_out"]], quote=FALSE, sep='\t', col.names = NA)
