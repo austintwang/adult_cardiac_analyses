35cbc1d82114f81356febe032b334c98225b1834
Austin <austin.wang1357@gmail.com>
2022-09-15 13:11:45 -0400

    debug

--- workflow/scripts/seurat_convert_teichmann.R
+++ workflow/scripts/seurat_convert_teichmann.R
@@ -9,9 +9,6 @@ library(Seurat)
 library(patchwork)
 library(ggplot2)
 
-library(SeuratData, lib.loc=input_paths[["azimuth_library_dir"]])
-library(SeuratDisk, lib.loc=input_paths[["seuratdisk_library_dir"]])
-
 params <- snakemake@params
 input_paths <- snakemake@input
 output_paths <- snakemake@output
@@ -21,6 +18,8 @@ set.seed(params[["seed"]])
 
 Convert(input_paths[["h5ad"]], dest = output_paths[["h5seurat"]])
 
+library(SeuratData, lib.loc=input_paths[["azimuth_library_dir"]])
+library(SeuratDisk, lib.loc=input_paths[["seuratdisk_library_dir"]])
 
 sink(type = "message")
 sink()
