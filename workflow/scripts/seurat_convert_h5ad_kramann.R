21e8293a7a9192e58bc4a86e5b3d21c98b3c93c4
Austin <austin.wang1357@gmail.com>
2022-09-21 14:59:22 -0400

    debug

--- workflow/scripts/seurat_convert_h5ad_kramann.R
+++ workflow/scripts/seurat_convert_h5ad_kramann.R
@@ -9,6 +9,7 @@ library(dplyr)
 library(Seurat)
 library(patchwork)
 library(ggplot2)
+library(Matrix)
 
 params <- snakemake@params
 input_paths <- snakemake@input
@@ -22,7 +23,7 @@ adata <- sc$read_h5ad(input_paths[["h5ad"]])
 adata ####
 
 # counts <- t(adata$layers["counts"])
-typeof(adata$X) ####
+# typeof(adata$X) ####
 counts <- t(adata$X)
 colnames(counts) <- adata$obs_names$to_list()
 rownames(counts) <- adata$var_names$to_list()
