fb831d2e5a62afc899e811cd3bcdd1b96ae7dd58
Austin <austin.wang1357@gmail.com>
2022-09-29 21:16:56 -0700

    debug

--- workflow/scripts/seurat_l2_labels_to_subgroups.R
+++ workflow/scripts/seurat_l2_labels_to_subgroups.R
@@ -17,11 +17,6 @@ log_paths = snakemake@log
 
 set.seed(params[["seed"]])
 
-read_fn <- function(path) {
-    sub_proj <- readRDS(file = path)
-    sub_proj@meta.data[, "cell_types_2", drop = FALSE]
-}
-
 plot_fn <- function(object, group, reduction, colors) {
     cats <- sort(unique(object@meta.data[[group]]))
     colors_out <- rep_len(colors, length(cats))
@@ -29,13 +24,10 @@ plot_fn <- function(object, group, reduction, colors) {
     DimPlot(proj_merged, reduction = reduction, group.by = group, label = TRUE, cols = colors_out, pt.size=0.1)
 }
 
-tables <- lapply(input_paths[["projects_subcluster"]], read_fn)
-head(tables[[1]])
-label_data <- do.call(rbind, tables)
-head(label_data) ####
+proj_l2 <- readRDS(file = input_paths[["project_integrated"]])
 
-proj <- readRDS(file = input_paths[["project_integrated"]])
-proj_merged <- AddMetaData(proj, label_data)
+proj <- readRDS(file = input_paths[["project_subgroup"]])
+proj_merged <- AddMetaData(proj, proj_l2@meta.data[, "cell_types_2", drop = FALSE])
 
 stallion <- c("1"="#D51F26","2"="#272E6A","3"="#208A42","4"="#89288F","5"="#F47D2B", "6"="#FEE500","7"="#8A9FD1","8"="#C06CAB","19"="#E6C2DC",
                "10"="#90D5E4", "11"="#89C75F","12"="#F37B7D","13"="#9983BD","14"="#D24B27","15"="#3BBCA8", "16"="#6E4B9E","17"="#0C727C", "18"="#7E1416","9"="#D8A767","20"="#3D3D3D")
