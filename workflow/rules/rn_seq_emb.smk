f91f012a02bfb48215fac463c8a953d55f5730bd
Austin <austin.wang1357@gmail.com>
2022-11-07 16:15:09 -0800

    import seq embeddings

---
+++ workflow/rules/rn_seq_emb.smk
@@ -0,0 +1,44 @@
+def get_seq_emb_cells(w):
+    region, status, sex = w.group.split("-")
+    region = region.upper()
+    sex = sex.upper()
+    return os.path.join(config["seq_emb_dir"], f"{status}_{region}_{sex}_cell_labels.txt")
+
+def get_seq_emb_scbasset(w):
+    region, status, sex = w.group.split("-")
+    region = region.upper()
+    sex = sex.upper()
+    return os.path.join(config["seq_emb_dir"], f"{status}_{region}_{sex}_scBasset_preharmony.mtx")
+
+def get_seq_emb_cellspace(w):
+    region, status, sex = w.group.split("-")
+    region = region.upper()
+    sex = sex.upper()
+    return os.path.join(config["seq_emb_dir"], f"{status}_{region}_{sex}_CellSpace_preharmony.mtx")
+
+
+rule seurat_add_seq_emb:
+    """
+    Add ATAC sequence embeddings from Salil and harmonize
+    """
+    input:
+        project_in = "results_groups/{group}/rna/seurat_add_batch_data/proj.rds",
+        cells = get_seq_emb_cells,
+        scbasset = get_seq_emb_scbasset,
+        cellspace = get_seq_emb_cellspace
+    output:
+        project_out = "results_groups/{group}/rna/seurat_add_seq_emb/proj.rds",
+        scbasset_harmony_mat = "results_groups/{group}/rna/seurat_add_seq_emb/scbasset_harmony_mat.mtx",
+        scbasset_harmony_rows = "results_groups/{group}/rna/seurat_add_seq_emb/scbasset_harmony_rows.txt",
+        scbasset_harmony_cols = "results_groups/{group}/rna/seurat_add_seq_emb/scbasset_harmony_cols.txt",
+        cellspace_harmony_mat = "results_groups/{group}/rna/seurat_add_seq_emb/cellspace_harmony_mat.mtx",
+        cellspace_harmony_rows = "results_groups/{group}/rna/seurat_add_seq_emb/cellspace_harmony_rows.txt",
+        cellspace_harmony_cols = "results_groups/{group}/rna/seurat_add_seq_emb/cellspace_harmony_cols.txt"
+    params:
+        seed = config["seurat_seed"],
+    log:
+        console = "logs/merged/{group}/rna/seurat_add_seq_emb/console.log"
+    conda:
+        "../envs/seurat.yaml"
+    script:
+        "../scripts/seurat_import_seq_embeddings.R"
---
+++ workflow/scripts/seurat_import_seq_embeddings.R
@@ -0,0 +1,64 @@
+Sys.setenv(CONDA_BUILD_SYSROOT="/")
+
+console_log <- file(snakemake@log[["console"]], open = "wt")
+sink(console_log)
+sink(console_log, type = "message")
+
+library(dplyr)
+library(Seurat)
+library(patchwork)
+library(ggplot2)
+library(Matrix)
+library(harmony)
+
+params = snakemake@params 
+input_paths = snakemake@input
+output_paths = snakemake@output
+log_paths = snakemake@log
+
+set.seed(params[["seed"]])
+
+# stallion <- c("1"="#D51F26","2"="#272E6A","3"="#208A42","4"="#89288F","5"="#F47D2B", "6"="#FEE500","7"="#8A9FD1","8"="#C06CAB","19"="#E6C2DC",
+#                "10"="#90D5E4", "11"="#89C75F","12"="#F37B7D","13"="#9983BD","14"="#D24B27","15"="#3BBCA8", "16"="#6E4B9E","17"="#0C727C", "18"="#7E1416","9"="#D8A767","20"="#3D3D3D")
+# stallion <- unname(stallion)
+
+# plot_fn <- function(object, group, reduction, colors) {
+#     cats <- sort(unique(object@meta.data[[group]]))
+#     colors_out <- rep_len(colors, length(cats))
+#     names(colors_out) <- cats
+#     DimPlot(object, reduction = reduction, group.by = group, label = TRUE, cols = colors_out, pt.size=0.1)
+# }
+
+t <- read.table(file = input_paths[["data"]], sep = '\t', header = TRUE, row.names = 1)
+
+cellnames <- unlist(readLines(input_paths[["cells"]]))
+
+mat_scbasset <- readMM(input_paths[["scbasset"]])
+rownames(mat_scbasset) <- cellnames
+
+mat_cellspace <- readMM(input_paths[["cellspace"]])
+rownames(mat_cellspace) <- cellnames
+
+proj <- readRDS(file = input_paths[["project_in"]])
+
+proj[["scbasset"]] <- CreateDimReducObject(embeddings = mat_scbasset, key = "SCB_")
+proj[["cellspace"]] <- CreateDimReducObject(embeddings = mat_scbasset, key = "CSP_")
+
+proj <- RunHarmony(proj, "dataset", assay.use = "RNA_train", reduction = "scbasset", reduction.save = "scbasset_harmony")
+proj <- RunHarmony(proj, "dataset", assay.use = "RNA_train", reduction = "cellspace", reduction.save = "cellspace_harmony")
+
+scbasset_int_data <- Embeddings(proj, reduction = "scbasset_harmony")
+cellspace_int_data <- Embeddings(proj, reduction = "cellspace_harmony")
+
+writeMM(scbasset_int_data, output_paths[["scbasset_harmony_mat"]])
+writeLines(rownames(scbasset_int_data), con = output_paths[["scbasset_harmony_rows"]])
+writeLines(colnames(scbasset_int_data), con = output_paths[["scbasset_harmony_cols"]])
+
+writeMM(cellspace_int_data, output_paths[["scbasset_cellspace_mat"]])
+writeLines(rownames(cellspace_int_data), con = output_paths[["scbasset_cellspace_rows"]])
+writeLines(colnames(cellspace_int_data), con = output_paths[["scbasset_cellspace_cols"]])
+
+# plt <- plot_fn(proj, "bc_counts", "umap", stallion) + labs(title="Barcode counts")
+# ggsave(output_paths[["umap"]], plt, device = "pdf")
+
+saveRDS(proj, file = output_paths[["project_out"]])
