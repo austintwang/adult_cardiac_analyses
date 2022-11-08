7b7de545bf12f89c5207a013c0146cdc2187cee8
Austin <austin.wang1357@gmail.com>
2022-11-07 20:22:28 -0800

    debug

--- workflow/scripts/seurat_import_seq_embeddings.R
+++ workflow/scripts/seurat_import_seq_embeddings.R
@@ -48,8 +48,11 @@ mat_cellspace <- mat_cellspace[idx,,drop=FALSE]
 
 head(mat_cellspace) ####
 
-proj[["scbasset"]] <- CreateDimReducObject(embeddings = mat_scbasset, key = "SCB_")
-proj[["cellspace"]] <- CreateDimReducObject(embeddings = mat_cellspace, key = "CSP_")
+a <- CreateDimReducObject(embeddings = mat_scbasset, key = "SCB_", assay = "RNA_train")
+proj[["scbasset"]] <- a
+print(a) ####
+b <- CreateDimReducObject(embeddings = mat_cellspace, key = "CSP_", , assay = "RNA_train")
+proj[["cellspace"]] <- b
 
 proj <- RunHarmony(proj, "dataset", assay.use = "RNA_train", reduction = "scbasset", reduction.save = "scbasset_harmony")
 proj <- RunHarmony(proj, "dataset", assay.use = "RNA_train", reduction = "cellspace", reduction.save = "cellspace_harmony")
