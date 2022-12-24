de210036f08dab5691884ac1c6612db24374c0fc
Austin <austin.wang1357@gmail.com>
2022-12-24 12:37:30 -0800

    debug

--- workflow/scripts/seurat_plot_genes.R
+++ workflow/scripts/seurat_plot_genes.R
@@ -16,6 +16,17 @@ log_paths = snakemake@log
 
 set.seed(params[["seed"]])
 
+stallion <- c("1"="#D51F26","2"="#272E6A","3"="#208A42","4"="#89288F","5"="#F47D2B", "6"="#FEE500","7"="#8A9FD1","8"="#C06CAB","19"="#E6C2DC",
+               "10"="#90D5E4", "11"="#89C75F","12"="#F37B7D","13"="#9983BD","14"="#D24B27","15"="#3BBCA8", "16"="#6E4B9E","17"="#0C727C", "18"="#7E1416","9"="#D8A767","20"="#3D3D3D")
+stallion <- unname(stallion)
+
+plot_fn <- function(object, group, reduction, colors) {
+    cats <- sort(unique(object@meta.data[[group]]))
+    colors_out <- rep_len(colors, length(cats))
+    names(colors_out) <- cats
+    DimPlot(object, reduction = reduction, group.by = group, label = TRUE, cols = colors_out, pt.size=0.1)
+}
+
 genes <- read.table(file = params[["genes"]], sep = '\t', header = TRUE, comment.char = "")
 head(genes) ####
 proj <- readRDS(file = input_paths[["project_in"]])
