e41b607a15982b9adc8f6e5c24189bc5a7739adc
Austin <austin.wang1357@gmail.com>
2023-01-27 22:24:53 -0800

    debug

--- workflow/scripts/seurat_plot_subcluster_genes.R
+++ workflow/scripts/seurat_plot_subcluster_genes.R
@@ -53,5 +53,5 @@ for (index in seq_len(nrow(genes))) {
 plt <- DotPlot(proj, features = genes[,2]) + RotatedAxis()
 ggsave(output_paths[["dotplot"]], plt, device = "pdf", width = 11, height = 7)
 
-plt <- DoHeatmap(subset(proj, downsample = 100), features = genes[,2], size = 3)
-ggsave(output_paths[["heatmap"]], plt, device = "pdf", width = 10, height = 5, limitsize = FALSE, raster = FALSE)
+plt <- DoHeatmap(subset(proj, downsample = 100), features = genes[,2], size = 3, raster = FALSE)
+ggsave(output_paths[["heatmap"]], plt, device = "pdf", width = 10, height = 5, limitsize = FALSE)
