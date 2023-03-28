20207cd9d7458a84e60f7690e9f2e6d87e4258c0
Austin <austin.wang1357@gmail.com>
2023-03-28 15:03:03 -0700

    debug

--- workflow/scripts/seurat_write_embeddings.R
+++ workflow/scripts/seurat_write_embeddings.R
@@ -6,6 +6,18 @@ sink(console_log, type = "message")
 
 library(Seurat)
 
+stallion <- c("1"="#D51F26","2"="#272E6A","3"="#208A42","4"="#89288F","5"="#F47D2B", "6"="#FEE500","7"="#8A9FD1","8"="#C06CAB","19"="#E6C2DC",
+               "10"="#90D5E4", "11"="#89C75F","12"="#F37B7D","13"="#9983BD","14"="#D24B27","15"="#3BBCA8", "16"="#6E4B9E","17"="#0C727C", "18"="#7E1416","9"="#D8A767","20"="#3D3D3D")
+stallion <- unname(stallion)
+
+plot_fn <- function(object, group, reduction, colors) {
+    cats <- sort(unique(object@meta.data[[group]]))
+    colors_out <- rep_len(colors, length(cats))
+    names(colors_out) <- cats
+    DimPlot(object, reduction = reduction, group.by = group, label = TRUE, cols = colors_out, pt.size=0.1)
+}
+
+
 params = snakemake@params 
 input_paths = snakemake@input
 output_paths = snakemake@output
