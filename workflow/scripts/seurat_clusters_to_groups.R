aabb963c37e7ef987caa4034f9891ac5b25b0496
Austin <austin.wang1357@gmail.com>
2023-04-02 21:30:26 -0700

    debug

--- workflow/scripts/seurat_clusters_to_groups.R
+++ workflow/scripts/seurat_clusters_to_groups.R
@@ -20,7 +20,7 @@ set.seed(params[["seed"]])
 read_fn <- function(path) {
     sub_proj <- readRDS(file = path)
     # print(head(sub_proj@meta.data)) ####
-    sub_proj@meta.data[, c("cell_types_l1", "cell_types_l2"), drop = FALSE]
+    sub_proj@meta.data[, "cell_types_l1", drop = FALSE]
 }
 
 plot_fn <- function(object, group, reduction, colors) {
