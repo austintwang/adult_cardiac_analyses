8258a86f31bad3a5452665f41fcf8f57320f50ef
Austin <austin.wang1357@gmail.com>
2023-06-09 15:21:38 -0700

    debug

--- workflow/scripts/collate_pseudobulks.R
+++ workflow/scripts/collate_pseudobulks.R
@@ -34,7 +34,9 @@ for (i in seq_along(mat_paths)) {
 
 mat_lst_aligned <- vector("list", length(mat_paths))
 for (i in seq_along(mat_paths)) {
-    mat_aln <- matrix(0, nrow = length(gene_names), ncol = ncol( mat_lst[[i]]))
+    mat_aln <- matrix(0, nrow = length(gene_names), ncol = ncol(mat_lst[[i]]))
+    rownames(mat_aln) <- gene_names
+    colnames(mat_aln) <- colnames(mat_lst[[i]])
     mat_aln[rownames(mat_lst[[i]]),] <- mat_lst[[i]]
     mat_lst_aligned[[i]] <- mat_aln
 }
