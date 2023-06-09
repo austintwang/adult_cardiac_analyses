9ead83bd92e9cb8da5482c08eb751316dbc55ac0
Austin <austin.wang1357@gmail.com>
2023-06-09 15:17:36 -0700

    debug

--- workflow/scripts/collate_pseudobulks.R
+++ workflow/scripts/collate_pseudobulks.R
@@ -14,6 +14,7 @@ log_paths = snakemake@log
 
 set.seed(params[["seed"]])
 
+
 mat_paths <- input_paths[["mats"]]
 metadata_paths <- input_paths[["metadata"]]
 
@@ -22,17 +23,26 @@ mat_lst <- vector("list", length(mat_paths))
 # print(length(mat_paths)[[1]]) ####
 # print(seq_along(length(mat_paths))) ####
 # print(seq_along(length(mat_paths)[[1]])) ####
-
+gene_names <- vector()
 for (i in seq_along(mat_paths)) {
     print(i) ####
     mat <- read.table(mat_paths[[i]], header = TRUE, sep = "\t", row.names = 1)
     mat_lst[[i]] <- mat
     print(head(mat)) ####
+    gene_names <- union(gene_names, rownames(mat))
 }
+
+mat_lst_aligned <- vector("list", length(mat_paths))
+for (i in seq_along(mat_paths)) {
+    mat_aln <- matrix(length(gene_names), ncol( mat_lst[[i]]))
+    mat_aln[rownames(mat_lst[[i]]),] <- mat_lst[[i]]
+    mat_lst_aligned[[i]] <- mat_aln
+}
+
 # print(mat_lst) ####
 
-cbind_op <- function(x, y) {cbind.fill(x, y, fill = 0)}
-mat_merged <- Reduce(cbind_op, mat_lst)
+# cbind_op <- function(x, y) {cbind.fill(x, y, fill = 0)}
+mat_merged <- do.call(cbind, mat_lst_aligned)
 write.table(mat_merged, file=output_paths[["mat"]], quote=FALSE, sep='\t', col.names = NA)
 
 meta_lst <- vector("list", length(metadata_paths))
